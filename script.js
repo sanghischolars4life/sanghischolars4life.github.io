f702b668fdb817e5532429c29aae90a44c6d290a
PandoPandu <pandopandumallandhi@gmail.com>
2024-08-09 12:59:40 -0700

    Added more pics

--- script.js
+++ script.js
@@ -13,30 +13,14 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const backgroundMusic = document.getElementById("background-music");
     let isMusicPlaying = false;
-    
+
     // Array of image paths
-    const imageArray = [
-        "images/img0.jpg", // First image shown when the PRESS button is clicked
-        "images/img1.jpg", 
-        "images/img2.jpg",
-        "images/img3.jpg",
-        "images/img4.jpg",
-        "images/img5.jpg",
-        "images/img6.jpg",
-        "images/img7.jpg",
-        "images/img8.jpg",
-        "images/img9.jpg",
-        "images/img10.jpg",
-        "images/img11.jpg", 
-        "images/img12.jpg",
-        "images/img13.jpg",
-        "images/img14.jpg",
-        "images/img15.jpg",
-        "images/img16.jpg",        
-        "images/img17.jpg",
-        "images/img18.jpg",
-        "images/img19.jpg",
-    ];
+    const imageArray = [];
+
+    for (let i = 0; i <= 50; i++) {
+        imageArray.push(`images/img${i}.jpg`);
+    }
+
 
     const complArray = [
         "You are so pretty", // First image shown when the PRESS button is clicked
